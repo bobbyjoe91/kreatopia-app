eab04d0920abb7fe3dd89155f84e4d7a42f36deb
bobbyjoe91 <bobby.cool00763@protonmail.com>
2021-08-07 19:33:31 +0700

    add artwork json data

--- lib/components/artwork.dart
+++ lib/components/artwork.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Artwork extends StatelessWidget {
+  final artworkData;
+
+  Artwork({ required this.artworkData });
 
   @override
   Widget build(BuildContext context) {
@@ -8,82 +11,98 @@ class Artwork extends StatelessWidget {
       height: 256,
       margin: EdgeInsets.symmetric(horizontal: 18),
       decoration: BoxDecoration(
+        color: Colors.white,
         border: Border.all(
           color: Color(0xFFFCD13A),
           width: 1
         ),
         borderRadius: BorderRadius.all(
           Radius.circular(10)
-        )
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.25),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 4)
+          )
+        ]
       ),
-      child: InkWell(
-        onTap: () => print('Tapped...'),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 156,
-              child: ClipRRect(
-                child: Image.network(
-                  'https://res.cloudinary.com/lelestaticassets/image/upload/v1627955035/Dicoding%20Final%20Project/4459586_lujgnw.jpg',
-                  fit: BoxFit.fitWidth
+      child: Material(
+        shape: RoundedRectangleBorder(
+          borderRadius:  BorderRadius.all(
+            Radius.circular(10)
+          )
+        ),
+        child: InkWell(
+          onTap: () => print('Tapped...'),
+          child: 
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: 156,
+                  child: ClipRRect(
+                    child: Image.network(
+                      artworkData['img_link'],
+                      fit: BoxFit.fitWidth
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)
+                    )
+                  ),
                 ),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10)
-                )
-              ),
-            ),
-            Container( // text container
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Teh Beautiful Spring',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w700
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 7),
-                    padding: EdgeInsets.only(top: 2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'by @kevpow',
-                          style: TextStyle(
-                            color: Color(0xFFC4C4C4)
-                          )
-                        ),
-                        Text(
-                          'Sat, 14 Aug 2021',
-                          style: TextStyle(
-                            color: Color(0xFFC4C4C4)
-                          )
-                        ),
-                      ]
-                    )
-                  ),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq...',
-                    textAlign: TextAlign.justify,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontFamily: 'Open Sans',
-                      fontSize: 12
-                    )
+                Container( // text container
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        artworkData['title'],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700
+                        )
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 7),
+                        padding: EdgeInsets.only(top: 2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'by ${artworkData['creator_id']}',
+                              style: TextStyle(
+                                color: Color(0xFFC4C4C4)
+                              )
+                            ),
+                            Text(
+                              artworkData['created_at'],
+                              style: TextStyle(
+                                color: Color(0xFFC4C4C4)
+                              )
+                            ),
+                          ]
+                        )
+                      ),
+                      Text(
+                        artworkData['description'],
+                        textAlign: TextAlign.justify,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontFamily: 'Open Sans',
+                          fontSize: 12
+                        )
+                      )
+                    ],
                   )
-                ],
-              )
+                )
+              ],
             )
-          ],
-        )
       )
-    );
+    ));
   }
 }
--- lib/pages/landingPage.dart
+++ lib/pages/landingPage.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'dart:convert';
+
+// components
 import '../components/artwork.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPage createState() => _LandingPage();
+}
+class _LandingPage extends State<LandingPage> {
+  // const [artworkDataList, setArtworkDataList] = useState([])
+  var _artworkDataList = [];
+
+  Future<void> readJson() async {
+    String response = await rootBundle.loadString('assets/data/artwork.json');
+    var data = await json.decode(response);
+
+    setState(() => _artworkDataList = data);
+  }
+
+  @override
+  void initState() {
+    readJson(); // read json data
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Kreatopia")
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Artwork(),
-            Artwork(),
-            Artwork(),
-            Artwork(),
-            Artwork(),
-            Artwork()
-          ],
-        ),
+      body: ListView.separated(
+        itemCount: _artworkDataList.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 30
+          );
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.only(
+              top: (index == 0) ? 30 : 0
+            ),
+            child: Artwork(
+              artworkData: _artworkDataList[index]
+            )
+          );
+        }
       )
     );
   }
