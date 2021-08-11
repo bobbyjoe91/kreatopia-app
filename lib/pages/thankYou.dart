9d0faa2c121bc1db0f15acc97c43ed07ff615a77
bobbyjoe91 <bobby.cool00763@protonmail.com>
2021-08-11 08:49:34 +0700

    develop thank you page

---
+++ lib/pages/thankYou.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ThankYou extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kreatopia')
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Thank you')
+          ],
+        )
+      )
+    );
+  }
+}
